da70d5ed395f8ba9773b811b016e3cb10f3eb7de
emab <eddybrown@me.com>
2022-01-11 15:38:33 +0000

    finally get katex working

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,47 @@
 module.exports = {
-  pathPrefix: '/brain-backup',
+  pathPrefix: "/brain-backup",
   siteMetadata: {
-    title: 'Brain Backup',
+    title: "Brain Backup"
   },
   plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-sharp`,
+    `gatsby-remark-images`,
     {
-      resolve: 'gatsby-philipps-foam-theme',
+      resolve: `gatsby-plugin-mdx`,
       options: {
-        contentPath: 'content/garden',
-        rootNote: "home",
-      },
+        extensions: [`.md`, `.mdx`],
+        remarkPlugins: [
+          require("remark-math"),
+        ],
+        rehypePlugins: [
+          require("rehype-katex")
+        ],
+        gatsbyRemarkPlugins: [
+          `gatsby-remark-foam-links`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 561
+            }
+          },
+          `gatsby-remark-copy-linked-files`,
+          {
+            resolve: `gatsby-remark-autolink-headers`,
+            options: {
+              icon: false
+            }
+          }
+        ]
+      }
     },
-  ],
-};
+    {
+      resolve: `gatsby-philipps-foam-theme`,
+      options: {
+        mdxOtherwiseConfigured: true,
+        contentPath: "content/garden",
+        rootNote: "home"
+      }
+    }
+  ]
+}
